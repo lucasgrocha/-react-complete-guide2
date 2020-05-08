cd76fdd2f63d4d4f493a1c467306b2da2dcfab4b
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-05-08 14:11:54 -0300

    66. Setting Styles Dynamically

--- src/App.js
+++ src/App.js
@@ -45,7 +45,8 @@ class App extends Component {
   render() {
 
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
@@ -68,6 +69,8 @@ class App extends Component {
           })}
         </div>
       )
+
+      style.backgroundColor = 'red'
     }
 
     return (
@@ -76,7 +79,7 @@ class App extends Component {
         <p>This is really working</p>
         <button
           style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
+          onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
