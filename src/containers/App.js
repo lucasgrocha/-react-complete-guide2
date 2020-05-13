aa24685dc2486d6f0eda41dd1f9f9e9222c4c6ac
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-05-12 23:15:41 -0300

    92. Component Update Lifecycle (for state Changes)

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,6 +36,15 @@ class App extends Component {
     console.log('[App.js] componentDidMount')
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[App.js] shouldComponentUpdate')
+    return true
+  }
+
+  componentDidUpdate() {
+    console.log('[App.js] componentDidUpdate')
+  }
+
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(person => {
       return person.id === id
